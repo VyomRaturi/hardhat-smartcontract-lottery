402e2ee7657d2e6358dfe9a75aa4c5f8d574fa32
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-22 13:06:32 +0530

    getting random winner

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,21 +7,41 @@ import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interface
 contract Raffle is VRFConsumerBaseV2 {
     // Errors
     error Raffle__LessEthSent();
+    error Raffle__EthTransferFailed();
 
-    // State variables
+    // state variables
     uint256 private immutable i_fees;
+    // uint256 private immutable i_interval;
+    // uint256 private s_lastTimestamp;
     address[] private s_participants;
+    // mapping(address user => uint256 lastRoundPlayed) private userRound;
+    // uint256 private s_round;
+    address private s_recentWinner;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     // Events
     event EnteredRaflle(address indexed user);
+    event RequestedRandomWords(uint256 indexed requestId);
+    event WinnerSelected(address indexed winner);
 
     constructor(
         address vrfCoordinatorAddr,
-        uint256 entranceFee
+        uint256 fees,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorAddr) {
-        i_fees = entranceFee;
+        i_fees = fees;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorAddr);
+        i_gasLane = gasLane;
+        i_subId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaflle() public payable {
@@ -33,19 +53,19 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function fulfillRandomWords(
-        uint256 /* requestId */,
+        uint256 /*requestId*/,
         uint256[] memory randomWords
     ) internal override {
-        //     uint256 randomNumber = randomWords[0] % s_participants.length;
-        //     address winner = s_participants[randomNumber];
-        //     emit WinnerSelected(winner, address(this).balance, s_round);
-        //     (bool success, ) = payable(winner).call{value: address(this).balance}(
-        //         ""
-        //     );
-        //     if (!success) {
-        //         revert Raffle__EthTransferFailed();
-        //     }
-        //     s_recentWinner = winner;
+        uint256 randomNumber = randomWords[0] % s_participants.length;
+        address winner = s_participants[randomNumber];
+        emit WinnerSelected(winner);
+        (bool success, ) = payable(winner).call{value: address(this).balance}(
+            ""
+        );
+        if (!success) {
+            revert Raffle__EthTransferFailed();
+        }
+        s_recentWinner = winner;
         //     s_round += 1;
         //     s_participants = new address[](0);
         //     s_lastTimestamp = block.timestamp;
@@ -67,5 +87,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getParticipantsLength() external view returns (uint256) {
         return s_participants.length;
+    }
+
+    function getRecentWinner() external view returns (address) {
+        return s_recentWinner;
     }
 }
